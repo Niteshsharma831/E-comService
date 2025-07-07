99bb07134c496a2dfbc8b6d843f8f8af09b08405
NItesh Kumar Sharma <niteshkumarsharma831@gmail.com>
2025-07-07 16:53:23 +0530

    add to cart updates

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -74,9 +74,9 @@ const loginUser = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: false, // ✅ true in production with HTTPS
-        sameSite: "Lax",
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        secure: true,
+        sameSite: "None",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .status(200)
       .json({
