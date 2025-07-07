a3b2faa9f35bcce560e89024302b09d853e41e55
NItesh Kumar Sharma <niteshkumarsharma831@gmail.com>
2025-07-07 17:03:58 +0530

    admin

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -75,7 +75,7 @@ const loginAdmin = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "Lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
