53fee40371e8f79c68331b650a7c8885bb66cf1a
NItesh Kumar Sharma <niteshkumarsharma831@gmail.com>
2025-07-08 14:22:48 +0530

    feat: add @vercel/speed-insights for performance tracking in the application

--- E-comService/src/App.jsx
+++ E-comService/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -71,6 +72,7 @@ const AppContent = () => {
     <>
       <ScrollToTop />
       <Analytics />
+      <SpeedInsights />
       {!isAdmin && <Navbar />}
       <Routes>
         {/* Public Routes */}
